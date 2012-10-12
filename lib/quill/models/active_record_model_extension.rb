66d2737381ba3786eea321f855eefb8510f4b9dc
David Jairala <davidjairala@gmail.com>
2012-10-12 10:28:10 -0700

    scope instance methods correctly inside qpage

--- lib/quill/models/active_record_model_extension.rb
+++ lib/quill/models/active_record_model_extension.rb
@@ -6,22 +6,32 @@ module Quill
     included do
 
       self.send(:include, Quill::ConfigurationMethods)
-      self.send(:include, Quill::PageScopeMethods)
 
     end
 
     module ClassMethods
 
       def qpage(num)
-        self.previous_page  = (num.to_i > 1)
-        self.limit_value    = default_per_page
-        self.offset_value   = self.limit_value * ([num.to_i, 1].max - 1)
+        offset_value    = default_per_page * ([num.to_i, 1].max - 1)
+        previous_page   = (num.to_i > 1)
+        limit_value     = default_per_page
+        offset_value    = offset_value
 
-        scope = limit(default_big_window).offset(self.offset_value)
+        scope = limit(default_big_window).offset(offset_value)
 
-        self.next_page      = (scope.size == default_big_window)
+        # set next page if available
+        next_page = (scope.size == default_big_window)
 
-        scope.limit(default_per_page)
+        scope = scope.except(:limit).limit(default_per_page)
+
+        scope.extend Quill::PageScopeMethods
+
+        scope.previous_page  = previous_page
+        scope.limit_value    = limit_value
+        scope.offset_value   = offset_value
+        scope.next_page      = next_page
+
+        return scope
       end
 
     end
--- lib/quill/models/page_scope_methods.rb
+++ lib/quill/models/page_scope_methods.rb
@@ -3,82 +3,78 @@ module Quill
 
     extend ActiveSupport::Concern
 
-    module ClassMethods
+    # Determines if there is a next page
+    def next_page?
+      !!@_q_next_page
+    end
 
-      # Determines if there is a next page
-      def next_page?
-        !!@_q_next_page
+    # Next page setter
+    def next_page=(val)
+      @_q_next_page = val
+    end
+
+    # Determines if there is a previous page
+    def previous_page?
+      !!@_q_previous_page
+    end
+
+    # Previous page setter
+    def previous_page=(val)
+      @_q_previous_page = val
+    end
+
+    # Total number of items in the set
+    def total_count(column_name = nil, options = {})
+      @total_count ||= begin
+        counter = except(:offset, :limit, :order, :includes)
+        counter = counter.count(column_name, options)
+
+        total_count = counter.count if counter.is_a?(ActiveSupport::OrderedHash)
+        total_count = counter.count(column_name, options) if counter.respond_to?(:count)
+        total_count = counter
+
+        total_count
       end
+    end
 
-      # Next page setter
-      def next_page=(val)
-        @_q_next_page = val
-      end
+    # Total number of pages
+    def total_pages
+      (total_count.to_f / limit_value).ceil
+    end
 
-      # Determines if there is a previous page
-      def previous_page?
-        !!@_q_previous_page
-      end
+    # Current page number
+    def current_page
+      (offset_value / limit_value) + 1
+    end
 
-      # Previous page setter
-      def previous_page=(val)
-        @_q_previous_page = val
-      end
+    # First page of the collection ?
+    def first_page?
+      current_page == 1
+    end
 
-      # Total number of items in the set
-      def total_count(column_name = nil, options = {})
-        @total_count ||= begin
-          counter = except(:offset, :limit, :order, :includes)
-          counter = counter.count(column_name, options)
+    # Last page of the collection?
+    def last_page?
+      current_page >= total_pages
+    end
 
-          total_count = counter.count if counter.is_a?(ActiveSupport::OrderedHash)
-          total_count = counter.count(column_name, options) if counter.respond_to?(:count)
-          total_count = counter
+    # Limit of the pagination
+    def limit_value
+      @_q_limit_value || default_per_page
+    end
 
-          total_count
-        end
-      end
+    # Limit setter
+    def limit_value=(val)
+      @_q_limit_value = val
+    end
 
-      # Total number of pages
-      def total_pages
-        (total_count.to_f / limit_value).ceil
-      end
+    # Offset of the pagination
+    def offset_value
+      @_q_offset_value || 0
+    end
 
-      # Current page number
-      def current_page
-        (offset_value / limit_value) + 1
-      end
-
-      # First page of the collection ?
-      def first_page?
-        current_page == 1
-      end
-
-      # Last page of the collection?
-      def last_page?
-        current_page >= total_pages
-      end
-
-      # Limit of the pagination
-      def limit_value
-        @_q_limit_value || default_per_page
-      end
-
-      # Limit setter
-      def limit_value=(val)
-        @_q_limit_value = val
-      end
-
-      # Offset of the pagination
-      def offset_value
-        @_q_offset_value || 0
-      end
-
-      # Limit setter
-      def offset_value=(val)
-        @_q_offset_value = val
-      end
-
+    # Limit setter
+    def offset_value=(val)
+      @_q_offset_value = val
     end
 
   end
